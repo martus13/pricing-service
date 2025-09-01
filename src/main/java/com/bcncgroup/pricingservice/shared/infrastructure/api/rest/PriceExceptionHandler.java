aba15e0ec762d147c30524e5e005b5bc2a80316d
Marta <marcaroli1@gmail.com>
2025-09-02 00:08:55 +0200

    feat(PriceExceptionHandler): añadir excepciones

--- src/main/java/com/bcncgroup/pricingservice/shared/infrastructure/api/rest/PriceExceptionHandler.java
+++ src/main/java/com/bcncgroup/pricingservice/shared/infrastructure/api/rest/PriceExceptionHandler.java
@@ -5,8 +5,11 @@ import com.bcncgroup.pricingservice.shared.infrastructure.api.rest.models.Proble
 import lombok.extern.jbosslog.JBossLog;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @JBossLog
 @ControllerAdvice
@@ -21,6 +24,39 @@ public class PriceExceptionHandler {
                         String.valueOf(HttpStatus.NOT_FOUND.value()),
                         "Price Not Found",
                         e.getMessage()));
+    }
+
+    @ExceptionHandler(value = MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ProblemDetails> handle(MethodArgumentTypeMismatchException e) {
+        log.debug(e.getMessage(), e);
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ProblemDetails(
+                        String.valueOf(HttpStatus.BAD_REQUEST.value()),
+                        "Petición no válida",
+                        "La petición enviada no tiene el formato adecuado"));
+    }
+
+    @ExceptionHandler(value = MissingServletRequestParameterException.class)
+    public ResponseEntity<ProblemDetails> handle(MissingServletRequestParameterException e) {
+        log.debug(e.getMessage(), e);
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ProblemDetails(
+                        String.valueOf(HttpStatus.BAD_REQUEST.value()),
+                        "Petición no válida",
+                        "Faltan parámetros en la petición"));
+    }
+
+    @ExceptionHandler(value = HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<ProblemDetails> handle(HttpRequestMethodNotSupportedException e) {
+        log.warn("Method Not Allowed", e);
+        return ResponseEntity
+                .status(HttpStatus.METHOD_NOT_ALLOWED)
+                .body(new ProblemDetails(
+                        String.valueOf(HttpStatus.METHOD_NOT_ALLOWED.value()),
+                        "Method Not Allowed",
+                        String.format("Request method '%s' not supported", e.getMethod())));
     }
 
     @ExceptionHandler(value = Exception.class)
