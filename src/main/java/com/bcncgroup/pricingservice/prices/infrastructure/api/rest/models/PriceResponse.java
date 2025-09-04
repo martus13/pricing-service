13f5e045a32b587fdd078ac8e24865fcf459261c
Marta <marcaroli1@gmail.com>
2025-09-04 17:41:15 +0200

    refactor: lenguaje y builder

--- src/main/java/com/bcncgroup/pricingservice/prices/infrastructure/api/rest/models/PriceResponse.java
+++ src/main/java/com/bcncgroup/pricingservice/prices/infrastructure/api/rest/models/PriceResponse.java
@@ -7,7 +7,7 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Data
-@Builder
+@Builder(toBuilder = true)
 public class PriceResponse {
     private Long brandId;
     private LocalDateTime startDate;
